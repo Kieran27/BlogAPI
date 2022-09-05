e3558495bb34bba90154111fd2081e3c2fb67090
Kieran Singh <kieransingh27@gmail.com>
2022-09-05 19:47:31 +1000

    Removed second app.listen function which was causing a conflict

--- app.js
+++ app.js
@@ -59,10 +59,4 @@ app.use((err, req, res, next) => {
   res.json({ error: err.message });
 });
 
-const port = process.env.PORT || 8000;
-
-app.listen(3000, () => {
-  console.log("Server Running on port " + port);
-});
-
 module.exports = app;
