9b118ad4da4ee502b4081119294df52604fe67bb
Kieran Singh <kieransingh27@gmail.com>
2022-09-05 18:10:53 +1000

    Changed port for deployment

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 
 // Set up mongoose
 const mongoose = require("mongoose");
@@ -28,6 +30,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(cors());
+app.use(compression());
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -55,8 +59,10 @@ app.use((err, req, res, next) => {
   res.json({ error: err.message });
 });
 
-app.listen(5000, () => {
-  console.log("App running on port 5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("Server Running on port " + port);
 });
 
 module.exports = app;
